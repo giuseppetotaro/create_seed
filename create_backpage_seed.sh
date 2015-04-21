ca80b1961c1158747df6e4b6ebcaed98ebc3963f
Giuseppe Totaro <totaropeppe@gmail.com>
2015-04-21 11:10:29 -0700

    Add support for comment lines
    Add support for comment lines

--- create_backpage_seed.sh
+++ create_backpage_seed.sh
@@ -4,17 +4,16 @@
 # Usage      : ./create_backpage_seed.sh /path/to/keywords /path/to/output
 # Author     : Giuseppe Totaro
 # Date       : 04-14-2015 [MM-DD-YYYY]
-# Last Edited: 04-14-2015, Giuseppe Totaro
+# Last Edited: 04-17-2015, Giuseppe Totaro
 # Description: This script creates a seed list using a combination of US 
 #              backpage urls and keywords.
-#              extract URLs from text extracted using Apache Tika.
 # Notes      : keywords file must include one keyword per line.
 #
 
 if [ $# -lt 2 ] || [ ! -f $1 ] || [ -e $2 ]
 then
 	echo "Usage: $0 /path/to/keywords /path/to/output"
-	echo "Be sure that the output file does not already exist!"
+	echo "NOTE: Be sure that the output file does not already exist!"
 	exit 1
 fi
 
@@ -26,6 +25,8 @@ for url in $(curl -s http://www.backpage.com/ | sed -n '/<div class=\"united-sta
 do
 	while read line
 	do
+		# ignores if line starts with # (comment)
+		[[ $line == "#"* ]] && continue
 		echo ${url}${QUERY}${line// /%20} >> "$OUTPUT"
 	done < "$KEYWORDS"
 done
